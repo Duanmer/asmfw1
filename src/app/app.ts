995198be58fa5ffe382382bceae0516b16b25bb5
Duanmer <duanpham0109@gmail.com>
2025-07-16 13:44:23 +0700

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductList } from "./product-list/product-list";
+import { CategoryList } from "./category-list/category-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductList, CategoryList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
